ecf4af48851ef174cf4316a06a6c6e16ff08a6a5
sago35 <sago35@gmail.com>
2015-12-10 20:55:17 +0900

    手動で対戦できるManual.pmを登録

---
+++ lib/PerlNabe/Sanmoku/Manual.pm
@@ -0,0 +1,29 @@
+package PerlNabe::Sanmoku::Manual;
+use 5.008001;
+use strict;
+use warnings;
+use utf8;
+use Moo;
+use Term::ReadKey;
+use Time::HiRes;
+
+extends 'PerlNabe::Sanmoku::Base';
+
+# Manual.pmを使う時は、wait = 0 がお勧め
+# perl -Ilib script/sanmoku.pl Manual Sample 0
+
+sub calc_next {
+    my $key;
+
+    while (1) {
+        while (not defined ($key = ReadKey(-1))) {
+            Time::HiRes::sleep(0.1);
+        }
+
+        if ($key =~ /^\d$/) {
+            return int $key;
+        }
+    }
+}
+
+1;
