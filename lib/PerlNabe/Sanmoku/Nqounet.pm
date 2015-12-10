55f6eccda7db197d3f67d7b2bd80983715b45d59
nqounet <mail@nqou.net>
2015-12-10 21:28:07 +0900

    refactor(master)

--- lib/PerlNabe/Sanmoku/Nqounet.pm
+++ lib/PerlNabe/Sanmoku/Nqounet.pm
@@ -14,54 +14,22 @@ sub calc_next {
     my @data = @_;
 
     # パターンを定義
+    my @lines = ([0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]);
     my @pattern;
-    push @pattern,
-      {
-        ban => [@data[0, 1, 2]],
-        key => [0,       1, 2]
-      };
-    push @pattern,
-      {
-        ban => [@data[3, 4, 5]],
-        key => [3,       4, 5]
-      };
-    push @pattern,
-      {
-        ban => [@data[6, 7, 8]],
-        key => [6,       7, 8]
-      };
-    push @pattern,
-      {
-        ban => [@data[0, 4, 8]],
-        key => [0,       4, 8]
-      };
-    push @pattern,
-      {
-        ban => [@data[2, 4, 6]],
-        key => [2,       4, 6]
-      };
-    push @pattern,
-      {
-        ban => [@data[0, 3, 6]],
-        key => [0,       3, 6]
-      };
-    push @pattern,
-      {
-        ban => [@data[1, 4, 7]],
-        key => [1,       4, 7]
-      };
-    push @pattern,
-      {
-        ban => [@data[2, 5, 8]],
-        key => [2,       5, 8]
-      };
+    for my $line (@lines) {
+        push @pattern,
+          +{
+            ban => [@data[@{$line}]],
+            key => $line,
+          };
+    }
 
     # 盤を評価
     my %score;
     for my $p (@pattern) {
         my $score = 0;
         map { $score++ unless $_ } @{$p->{ban}};
-        $score++ if all { $_ == 0 } @{$p->{ban}};
+        $score += 5 if all { $_ == 0 } @{$p->{ban}};
         my @p = grep { $_ == 1 } @{$p->{ban}};
         my @e = grep { $_ == 2 } @{$p->{ban}};
         $score += 20 if @p == 2;
@@ -83,6 +51,7 @@ sub calc_next {
         next if $score{$key} < $max;
         push @results, $key;
     }
+    p %score;
     my ($result) = shuffle @results;
     return $result;
 }
