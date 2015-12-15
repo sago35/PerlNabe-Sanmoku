829c52e065c3be72c03c8407c1b957e51f07763c
sago35 <sago35@gmail.com>
2015-12-15 22:02:28 +0900

    外部コマンドを実行するExec.pmを登録

---
+++ lib/PerlNabe/Sanmoku/Exec.pm
@@ -0,0 +1,31 @@
+package PerlNabe::Sanmoku::Exec;
+use 5.008001;
+use strict;
+use warnings;
+use utf8;
+use Moo;
+use Path::Tiny;
+
+extends 'PerlNabe::Sanmoku::Base';
+
+has cmd => (is => 'ro');
+
+sub calc_next {
+    my $self = shift;
+    my @data = @_;
+
+    my $t = Path::Tiny->tempfile;
+    printf "--- %s\n", $self->cmd;
+    open my $in, "|" . $self->cmd . "> $t" or croak $!;
+    printf $in "%s\n", join " ", @data[0..2];
+    printf $in "%s\n", join " ", @data[3..5];
+    printf $in "%s\n", join " ", @data[6..8];
+    close $in;
+
+    my $value = $t->slurp;
+    chomp $value;
+
+    return int $value;
+}
+
+1;
---
+++ lib/PerlNabe/Sanmoku/exec_sample.pl
@@ -0,0 +1,34 @@
+use strict;
+use warnings;
+use utf8;
+
+# Exec.pmで動かすための外部コマンド実装サンプルです
+#   標準入力からスペース区切りで以下を受け取ります
+#   順に$data[0]、$data[1]、というイメージです
+#
+#   0 0 0
+#   0 0 0
+#   0 0 0
+#
+#   自分が打つ手は、標準出力に出力してください
+#   真ん中に打つ場合は、以下の通りです
+#   改行文字の有無はどちらでも構いません
+#
+#   printf "%d\n", 4;
+#
+
+my @data;
+while (<>) {
+    push @data, (split /\s+/)[0 .. 2];
+}
+
+die if scalar @data != 9;
+
+foreach my $i (4, 0, 2, 6, 8, 1, 3, 5, 7) {
+    if ($data[$i] == 0) {
+        printf "%d\n", $i;
+        exit 0;
+    }
+}
+die;
+
