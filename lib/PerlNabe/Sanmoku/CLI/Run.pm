0787ca5ed07a68e0274df988f59e252043db6a4a
sago35 <sago35@gmail.com>
2016-01-11 13:53:32 +0900

    cliから実行するためのモジュールを登録

---
+++ lib/PerlNabe/Sanmoku/CLI/Run.pm
@@ -0,0 +1,39 @@
+package PerlNabe::Sanmoku::CLI::Run;
+use 5.008001;
+use strict;
+use warnings;
+use utf8;
+use Carp;
+use Getopt::Long qw(:config posix_default no_ignore_case gnu_compat);
+use PerlNabe::Sanmoku;
+
+sub run {
+    my $class = shift;
+    my @argv = @_;
+
+    my $options = {
+        nowait => 0,
+    };
+
+    my @params;
+
+    while (scalar @argv > 0) {
+        if ($argv[0] =~ /^-/) {
+            local @ARGV = @argv;
+            GetOptions($options,
+                "no-wait" => \$options->{nowait},
+            ) || confess "error";
+            @argv = @ARGV;
+        } else {
+            push @params, shift @argv;
+        }
+    }
+
+    my $sanmoku = PerlNabe::Sanmoku->new(
+        players => [@params[0, 1]],
+        wait    => !$options->{nowait},
+    );
+    $sanmoku->run;
+}
+
+1;
